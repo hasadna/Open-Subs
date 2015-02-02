8892b05a0ffde508b2ff06fcfca47efe997a2580
Benny Daon <bennydaon@gmail.com>
2015-02-02 14:19:39 +0200

    The candidate feed is almost here #8
    the code gets the candidate from open knesset and the feed from FB
    but for some reason, It doesn't display it on the page

--- angular/app/scripts/candidates.js
+++ angular/app/scripts/candidates.js
@@ -2,11 +2,35 @@
 
 angular.module('app')
 
-  .controller('CandidateController', function($resource, $scope, SETTINGS, $location, $routeParams, $window, OPEN_KNESSET) {
-    var person = OPEN_KNESSET.Person.get({id:$routeParams.id}, function () {
+  .controller('CandidateController', function($log, $scope, $routeParams, OPEN_KNESSET) {
+    OPEN_KNESSET.Person.get({id:$routeParams.id}, function (person) {
       $scope.person = person;
-      $scope.feed = [{content:'סטטוס ראשון'},
-                     {content:'סטטוס שני'}];
-      });
-  })
-;
+      var i, fb_page = "";
+      for (i=0; i < person.links.length; i++) {
+        if (person.links[i]["url"].search("facebook.com") > 0) {
+          fb_page = person.links[i]["url"];
+          break;
+        }
+      };
+      if (fb_page == "" && person.mk) {
+        for (i=0; i < person.mk.links.length; i++) {
+          if (person.mk.links[i]["url"].search("facebook.com") > 0) {
+            fb_page = person.mk.links[i]["url"];
+            break;
+          }
+        }
+      };
+      if (fb_page != "") {
+        var parts = fb_page.split('/'),
+            fb_id = parts[parts.length - 1];
+        $log.debug(fb_id);
+        FB.api("/"+fb_id+"/posts",
+            {access_token: "CAAEjpAqEyXcBAFJJvPisrbohjMMCQUJ414J9SkCrBhAEz9sfe5D1WVti10Sv2xhZCGH6TNEkZBzPRDbCL8sZAIfahEMyZB23db2s0jTcPMWZAVoL1Fcesi2Ch5bB4ZAgrA09SYwMZBtTrZBbEx2MkWulVktppUJNoVGsvYwmRv5coBoYbjAwk8kxCxWrkuvSoMZBn1dfZAQszxNgZDZD" },
+            function (response) {
+          if (response && !response.error) {
+            $scope.feed = response.data;
+          }
+        });
+      }
+    });
+  });
--- angular/app/scripts/open_knesset.js
+++ angular/app/scripts/open_knesset.js
@@ -12,6 +12,7 @@ angular.module('app')
        */
       Person: $resource(SETTINGS.oknesset+'/api/v2/person/:id/',
                           {id:'@id'}),
+      // TODO: get list of candidates from http://127.0.0.1:8000/api/v2/person/?format=json&roles__org=%D7%94%D7%91%D7%97%D7%99%D7%A8%D7%95%D7%AA%20%D7%9C%D7%9B%D7%A0%D7%A1%D7%AA%20%D7%94-20
     };
     return OPEN_KNESSET;
   })
