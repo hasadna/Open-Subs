0c8e63e21c068b9970b36750ff6ee27a15c07006
Benny Daon <bennydaon@gmail.com>
2015-02-03 17:09:34 +0200

    Electing and displaying the comittee chairs

--- angular/app/scripts/committee.js
+++ angular/app/scripts/committee.js
@@ -2,11 +2,25 @@
 
 angular
   .module('app')
-  .controller('CommitteeController', function($scope, OPEN_KNESSET, $routeParams) {
-    $scope.committee = OPEN_KNESSET.get_committee($routeParams.id);
+  .controller('CommitteeController', function($location, $scope, $window, OPEN_KNESSET, $routeParams) {
+    var committee_id = $routeParams.id
+    $scope.committee = OPEN_KNESSET.get_committee(committee_id);
     OPEN_KNESSET.get_candidates().success(function(data) {
       $scope.candidates = data.objects;
     });
+    $scope.$watch(function (scope) { return scope.selectedChair; },
+                  function (new_value, old_value) {
+      if (new_value) {
+        $window.sessionStorage.setItem('chair'+committee_id,
+              new_value.originalObject.id);
+        $location.path('/home');
+      }
+    });
+    $scope.elect = function (id) {
+        $window.sessionStorage.setItem('chair'+committee_id, id);
+        $location.path('/home');
+    };
+
   })
 ;
 
--- angular/app/scripts/home.js
+++ angular/app/scripts/home.js
@@ -2,12 +2,18 @@
 
 angular
   .module('app')
-  .controller('HomeController', function($scope, USER, OPEN_KNESSET, $location) {
+  .controller('HomeController', function($scope, USER, OPEN_KNESSET, $location, $window) {
     if (!USER.get()) {
       $location.path('/login');
     }
     else {
-      $scope.chairs = OPEN_KNESSET.committees;
+      $scope.chairs = []
+      for (var i=0; i < OPEN_KNESSET.committees.length; i++) {
+        var c = OPEN_KNESSET.committees[i];
+        $scope.chairs.push ({name: c.name, absolute_url: c.absolute_url,
+                            chosen: $window.sessionStorage.getItem('chair'+c.id)});
+
+      };
     }
   })
 ;
