65b687013700e855c3d98c14efe59273eeaa8614
Benny Daon <bennydaon@gmail.com>
2015-02-03 11:13:57 +0200

    first version of committee list #16

--- angular/app/scripts/home.js
+++ angular/app/scripts/home.js
@@ -2,9 +2,12 @@
 
 angular
   .module('app')
-  .controller('HomeController', function($scope, USER, $location) {
+  .controller('HomeController', function($scope, USER, OPEN_KNESSET, $location) {
     if (!USER.get()) {
       $location.path('/login');
+    }
+    else {
+      $scope.chairs = OPEN_KNESSET.committees;
     }
   })
 ;
