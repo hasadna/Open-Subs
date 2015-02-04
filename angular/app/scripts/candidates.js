2c412b44aa70b34a810ce3ad6bd78a83c7848b23
Ori Hoch <ori@uumpa.com>
2015-02-04 15:18:22 +0200

    let candidates use USER.fbapi for seamless integration with facebook

--- angular/app/scripts/candidates.js
+++ angular/app/scripts/candidates.js
@@ -2,16 +2,18 @@
 
 angular.module('app')
 
-  .controller('CandidateController', function($log, $scope, $routeParams, OPEN_KNESSET, ezfb) {
+  .controller('CandidateController', function($log, $scope, $routeParams, OPEN_KNESSET, USER) {
     OPEN_KNESSET.Person.get({id:$routeParams.id}, function (person) {
       $scope.person = person;
       var i, fb_page = "";
-      for (i=0; i < person.links.length; i++) {
-        if (person.links[i]["url"].search("facebook.com") > 0) {
-          fb_page = person.links[i]["url"];
-          break;
-        }
-      };
+      if (person.links) {
+        for (i=0; i < person.links.length; i++) {
+          if (person.links[i]["url"].search("facebook.com") > 0) {
+            fb_page = person.links[i]["url"];
+            break;
+          }
+        };
+      }
       if (fb_page == "" && person.mk) {
         for (i=0; i < person.mk.links.length; i++) {
           if (person.mk.links[i]["url"].search("facebook.com") > 0) {
@@ -24,11 +26,9 @@ angular.module('app')
         var parts = fb_page.split('/'),
             fb_id = parts[parts.length - 1];
         $log.debug(fb_id);
-        ezfb.api("/"+fb_id+"/posts",
-            {access_token: "CAAEjpAqEyXcBAEWjVamVN4S1LumbfJ7BLZBF2ZAj8L91jH1kqk3rw80eZAgDds5yXWxdmU9KqXI7F8QPKXSo5jbJZAsEtO7b76OvtvJfMTE5iZAUXDiq6s9ZA1SKFSGorJ8AiZCGr92oxJ2jlF6XYFqPR8BtJN9aNNMqMWZBmOBKZBBQawbAZAKviW4UtvuBFHT2lXLAngFzfTXAZDZD" },
-            function (response) {
-              $scope.feed = response;
-            });
+        USER.fbapi('/'+fb_id+'/posts').then(function(response) {
+          $scope.feed = response;
+        });
       }
     });
   });
