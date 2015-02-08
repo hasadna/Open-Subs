c11d5861ef8227d9291a12c7b37f44b42ddfef2d
Benny Daon <bennydaon@gmail.com>
2015-02-08 10:41:39 +0200

    Display the candidates by their party and position

--- angular/app/scripts/committee.js
+++ angular/app/scripts/committee.js
@@ -8,9 +8,42 @@ angular
       committee: OPEN_KNESSET.get_committee(committee_id),
       candidates: OPEN_KNESSET.get_candidates()
     }).then(function(res) {
+      var candidatesArray = $.map(res.candidates, function(v){return v;});
+      // build a candidates array ordered by group and then A-z
+      var i, j, orgs = {}, candidates = {};
+      for (i=0; i < candidatesArray.length; i++) {
+        var c = candidatesArray[i];
+        for (j=0; j < c.roles.length; j++) {
+          var role = c.roles[j];
+          if (role.org == "הבחירות לכנסת ה-20") {
+            var org = role.text.match(/מקום (\d+) ב(.+)/);
+            if (org !== null && org.length == 3){
+              c.ord = parseInt(org[1]);
+              org = org[2];
+              if (org in orgs)
+                candidates[org].push(c)
+              else {
+                orgs[org] = true;;
+                candidates[org] = [c];
+              }
+              // finished with this candidates, break to stop looping on roles
+              break;
+            }
+          }
+        }
+      }
+      var len = 0
+      for (var org in orgs) {
+        // var org = orgs[i];
+        candidates[org].sort(function (a, b) {
+          return a.ord - b.ord;
+        })
+        len++;
+      }
+      candidates.length = len;
+      $scope.candidatesArray = candidatesArray;
+      $scope.candidates = candidates;
       $scope.committee = res.committee;
-      $scope.candidatesArray = $.map(res.candidates, function(v){return v;});
-      $scope.candidates = res.candidates;
     });
     $scope.$watch(function (scope) { return scope.selectedChair; },
                   function (new_value, old_value) {
