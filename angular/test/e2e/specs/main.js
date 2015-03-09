9c3d522d62ee8149a66982556a6ee9530a3b91af
Benny Daon <bennydaon@gmail.com>
2015-03-09 17:47:05 +0200

    adding tests for `welcome` stage

--- angular/test/e2e/specs/main.js
+++ angular/test/e2e/specs/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var fail = function() { expect(true).toBe(false); }
+
 describe('when going to the root url', function() {
 
   beforeAll(function() {
+    // getting the root
     browser.get('/');
   });
 
@@ -16,17 +19,27 @@ describe('when going to the root url', function() {
   it('should show the splash screen', function() {
     expect(browser.driver.getCurrentUrl()).toMatch('/#/splash');
   });
+})
 
-  describe('when clicking the play button', function() {
+describe('when clicking the play button', function() {
+  beforeAll(function() {
+    // clicking play
+    element(by.css('div.splash a.play')).click();
+  });
 
-    beforeAll(function() {
-      element(by.css('div.splash a.play')).click();
-    });
+  it('should go to the game start page', function() {
+    expect(browser.driver.getCurrentUrl()).toMatch('/#/home');
+  });
 
-    it('should go to the game start page', function() {
-      expect(browser.driver.getCurrentUrl()).toMatch('/#/home');
-    });
+});
 
-  })
-
+describe('when clicking the elect button on the welcome page', function (){
+  beforeAll(function() {
+    // clicking drive
+    element(by.css('div.modal-dialog button.drive')).click();
+  });
+  it('should hide the help', function () {
+    browser.isElementPresent(element(by.css('section.sub')));
+    // expect(element(by.css('section.welcome')).visible()).toBe(false);
+  });
 });
